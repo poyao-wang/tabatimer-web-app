83d161b8ce9b5c7ffdf54c141d52fd7cff5647ca
poyaowang <michael90110@gmail.com>
2021-09-29 22:52:34 +0900

    Add MainContainerMid

---
+++ src/components/MainContainerMid.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MainContainerMid.css";
+
+const MainContainerMid: React.FC = ({ children }) => {
+  return <div className="main-container-mid">{children}</div>;
+};
+
+export default MainContainerMid;
