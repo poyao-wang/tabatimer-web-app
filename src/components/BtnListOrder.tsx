8bcc952873c873d7fac2bff3135ef8f431faeaec
poyaowang <michael90110@gmail.com>
2021-09-29 23:53:54 +0900

    Add BtnListOrder

---
+++ src/components/BtnListOrder.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./BtnListOrder.css";
+import Icon from "./Icon";
+
+const Up: React.FC = () => {
+  return (
+    <div className="btn-list-order">
+      <Icon.KeyboardArrowUp />
+    </div>
+  );
+};
+const Down: React.FC = () => {
+  return (
+    <div className="btn-list-order">
+      <Icon.KeyboardArrowDown />
+    </div>
+  );
+};
+const Top: React.FC = () => {
+  return (
+    <div className="btn-list-order">
+      <Icon.ArrowTop />
+    </div>
+  );
+};
+const Bottom: React.FC = () => {
+  return (
+    <div className="btn-list-order">
+      <Icon.ArrowBottom />
+    </div>
+  );
+};
+
+const BtnListOrder = {
+  Up: Up,
+  Down: Down,
+  Top: Top,
+  Bottom: Bottom,
+};
+
+export default BtnListOrder;
