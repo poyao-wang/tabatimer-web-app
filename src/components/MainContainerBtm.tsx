529789fd6fb247534e3b2e0f4ea3ab9d919f898a
poyaowang <michael90110@gmail.com>
2021-11-08 20:57:30 +0900

    Mod MainContainerBtm
    For btn-text and customClass support

--- src/components/MainContainerBtm.tsx
+++ src/components/MainContainerBtm.tsx
@@ -3,15 +3,19 @@ import "./MainContainerBtm.css";
 
 interface MainContainerBtmProps {
   show?: boolean;
+  customClassName?: string;
 }
 
 const MainContainerBtm: React.FC<MainContainerBtmProps> = ({
   show,
   children,
+  customClassName,
 }) => {
   if (show === undefined) show = true;
   const className =
-    "main-container-btm" + (show ? "" : " main-container-btm--hide");
+    "main-container-btm " +
+    (show ? "" : "main-container-btm--hide ") +
+    customClassName;
 
   return <div className={className}>{children}</div>;
 };
